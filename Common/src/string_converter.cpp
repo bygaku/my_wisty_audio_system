cdf795717a0f98a20cb6ce7896c1531a73bc7df8
intwisty <intmainwist@gmail.com>
2025-12-18 16:55:51 +0900

    Add: to_wstring and error output fuction.

---
+++ Common/src/string_converter.cpp
@@ -0,0 +1,8 @@
+#include "utility/string_converter.h"
+
+namespace wwist::utils {
+	std::wstring StringConverter::to_wstring(const std::string &str) {
+		std::wstring w_str(str.begin(), str.end());
+		return w_str;
+	}
+}
